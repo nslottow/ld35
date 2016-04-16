b6eac6cc445c93c40eef2a62c58c3b79b9136513
Nate Slottow <nslottow@gmail.com>
2016-04-16 09:50:52 -0700

    Added test map that uses a tileset created with TexturePacker. Removed TMX formatted map test. We're just going to use JSON, which you can also export directly from Tiled. Got map loading using Luxe's built-in Tiled importer.

--- src/Main.hx
+++ src/Main.hx
@@ -1,18 +1,31 @@
 import luxe.Vector;
 import luxe.Color;
+import luxe.Sprite;
 
 import luxe.importers.tiled.TiledMap;
 
+// TODO: Load tilemap
+
 class Main extends luxe.Game {
+
+	/** iPhone 6 point resolution */
+	public static inline var w_points:Int = 375;
+    public static inline var h_points:Int = 667;
+	public static var screen_size_points:Vector = new Vector(w_points, h_points);
+
+	public static var background:Sprite;
+	public static var background_color:Color = new Color(0.2, 0.2, 0.2);
+
 	override function config(config:luxe.AppConfig) {
 		config.preload = {
 			textures: [
-				{ id: 'assets/tilesets/8x8_dungeon.png' },
-				{ id: 'assets/tilesets/top_down_dungeon.png' }
+				{ id: 'assets/maps/tilesets/8x8_dungeon.png' },
+				{ id: 'assets/maps/tilesets/top_down_dungeon.png' },
+				{ id: 'assets/maps/tilesets/packer_test.png' },
 			],
 			texts: [
-				{ id: 'assets/maps/test_00.tmx' },
-				{ id: 'assets/maps/test_00.json' }
+				{ id: 'assets/maps/test_00.json' },
+				{ id: 'assets/maps/test_01.json' },
 			]
 		};
 
@@ -20,32 +33,36 @@ class Main extends luxe.Game {
 	}
 
 	override function ready() {
-		var map_id = 'assets/maps/test_00.tmx';
+		Luxe.camera.size = screen_size_points;
+        Luxe.camera.size_mode = luxe.Camera.SizeMode.fit;
+
+		background = new Sprite({
+			size: screen_size_points,
+			centered: false,
+			color: background_color,
+			no_scene: true
+		});
+
+		var map_id = 'assets/maps/test_00.json';
 		trace('Loading tiled map: $map_id');
 
 		var tiled_map = new TiledMap({
-			tiled_file_data: Luxe.resources.text(map_id).asset.text
+			asset_path: 'assets/maps',
+			tiled_file_data: Luxe.resources.text(map_id).asset.text,
+			format: 'json',
 		});
+
+		// Scale the tilemap so the whole thing fits horizontally
+		var scale:Float = w_points / (tiled_map.tile_width * tiled_map.width);
+		tiled_map.display({
+			scale: scale
+		});
+		trace('tile width = ${tiled_map.tile_width}, map scale = $scale, scaled tile width = ${tiled_map.tile_width * scale}');
+
+		var layer = tiled_map.layers_ordered[0];
+		var tile = layer.tiles[0][0];
+
+		tile.id = 0;
 	}
 }
 
-/**
-	Loaded from a Tiled map editor file
-	Coordinates are 0-based x,y from the top-left
-**/
-/*
-class Map {
-	var tiles:Array<Tile>;
-
-	public function new() {}
-
-	public function load_from_json(json_str:String) {
-	}
-
-	public inline function get_tile(x:Int, y:Int) {
-	}
-}
-
-class Tile {
-}
-*/
